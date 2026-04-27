83048e71398412952ad44298b71d557df3583548
Jake Coleman <wcoleman@sandiego.edu>
2026-04-27 16:42:13 -0700

    created all stubs for the current classes related to Customer class

---
+++ src/main/java/edu/sandiego/comp305/ServiceList.java
@@ -0,0 +1,17 @@
+package edu.sandiego.comp305;
+
+public class ServiceList {
+    List<Listing> listings;
+
+    List<Listing> getListing(){
+        return listings;
+    }
+
+    List<Listing> filterByService(String service){
+        return listings;
+    }
+
+    List<Listing> filterByPrice(String service, double maxPrice){
+        return listings;
+    }
+}
