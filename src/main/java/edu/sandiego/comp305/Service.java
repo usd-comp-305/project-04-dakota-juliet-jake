81636d7304b88fc458eb2b74e7cd853629e6c517
jroberto <jroberto@sandiego.edu>
2026-04-27 22:24:18 -0700

    feat: add stubs for Service class

---
+++ src/main/java/edu/sandiego/comp305/Service.java
@@ -0,0 +1,28 @@
+package edu.sandiego.comp305;
+
+public class Service {
+    private String name;
+    private double price;
+
+    public Service(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public void setName(String name){
+
+    }
+
+    public void setPrice(double price){
+
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+}
