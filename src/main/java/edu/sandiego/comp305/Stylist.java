c93a18ffd8530861b7aed818ffdfd20a2ffe9141
jroberto <jroberto@sandiego.edu>
2026-04-27 22:25:04 -0700

    feat: add stubs for Stylist class

---
+++ src/main/java/edu/sandiego/comp305/Stylist.java
@@ -0,0 +1,25 @@
+package edu.sandiego.comp305;
+
+public class Stylist {
+
+    public Stylist(){
+
+    }
+
+    public void fullDye(){
+
+    }
+
+    public void perm(){
+
+    }
+
+    public void highlights(){
+
+    }
+
+    public void blowout(){
+
+    }
+
+}
