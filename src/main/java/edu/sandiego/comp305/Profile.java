76a453338bdf336709ea2afa49838cffb0fe9d5a
jroberto <jroberto@sandiego.edu>
2026-05-03 20:50:01 -0700

    Feat: add test cases for profile class & adjust profile classes

--- src/main/java/edu/sandiego/comp305/Profile.java
+++ src/main/java/edu/sandiego/comp305/Profile.java
@@ -1,13 +1,48 @@
 package edu.sandiego.comp305;
 
 public abstract class Profile {
+
+    private String name;
+
     private String username;
 
     private String password;
 
-    private String name;
-
     private double rating;
+
+    Profile(){}
+
+    public void setName(String name){
+
+    }
+
+    public void setUsername(String username){
+
+    }
+
+    public void setPassword(String password){
+
+    }
+
+    public void adjustRating(double newRating){
+
+    }
+
+    public String getName(){
+
+    }
+
+    public String getUsername(){
+
+    }
+
+    public String getPassword(){
+
+    }
+
+    public double getRating(){
+
+    }
 
     public void cancelCall(){}
 }
---
+++ src/test/java/edu.sandiego.comp305/ProfileTest.java
@@ -0,0 +1,54 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class ProfileTest {
+
+    //need a concrete subclass to test everything
+    static class TestProfile extends Profile {
+        public TestProfile(){
+            super();
+        }
+    }
+
+    Profile profile = new TestProfile();
+
+    @Test
+    public void setNameTest(){
+        profile.setName("Lily Demman");
+        assertEquals("Lily Demman", profile.getName());
+    }
+
+    @Test
+    public void setUsernameTest(){
+        profile.setUsername("ldemman");
+        assertEquals("ldemman", profile.getUsername());
+    }
+
+    @Test
+    public void setPasswordPassTest(){
+        profile.setPassword("Passw0rd!");
+        assertEquals("Passw0rd!", profile.getPassword());
+    }
+
+    @Test
+    public void setPasswordFailTest(){
+        assertThrows(IllegalArgumentException.class, () -> profile.setPassword("password"));
+    }
+
+    @Test
+    public void adjustRatingTest(){
+        when(profile.getRating()).thenReturn(1.0);
+        profile.adjustRating(5.0);
+        assertEquals(3.0, profile.getRating());
+    }
+
+    @Test
+    public void adjustRatingFailedTest(){
+        assertThrows(IllegalArgumentException.class, () -> profile.adjustRating(6.0));
+    }
+}
