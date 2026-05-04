befb332b9f97b01b71f05339407ee6b8b388bf5d
jroberto <jroberto@sandiego.edu>
2026-05-03 20:00:02 -0700

    fix: checkstyle fixes

--- src/test/java/edu.sandiego.comp305/ServiceTest.java
+++ src/test/java/edu.sandiego.comp305/ServiceTest.java
@@ -1,10 +1,7 @@
 package edu.sandiego.comp305;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
 
 public class ServiceTest {
 
@@ -24,7 +21,7 @@ public class ServiceTest {
 
     @Test
     public void constructorTest(){
-        Service fastService = new Service("Barber", 25);
+        final Service fastService = new Service("Barber", 25);
         assertEquals("Barber", fastService.getName());
         assertEquals(25, fastService.getPrice());
     }
