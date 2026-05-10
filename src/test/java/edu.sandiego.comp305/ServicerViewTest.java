a41f5019f43a5b738c9705e67c40044bae091249
Jake Coleman <wcoleman@sandiego.edu>
2026-05-10 13:45:08 -0700

    created this branch with empty test for ServicerView

---
+++ src/test/java/edu.sandiego.comp305/ServicerViewTest.java
@@ -0,0 +1,28 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ServicerViewTest {
+
+    @Test
+    void setStrategy() {
+    }
+
+    @Test
+    void render() {
+    }
+
+    @Test
+    void showOfferedServices() {
+    }
+
+    @Test
+    void showSchedule() {
+    }
+
+    @Test
+    void showNotification() {
+    }
+}
