43ff21bc223aa669e315aa14781d5057e7db161d
Jake Coleman <wcoleman@sandiego.edu>
2026-05-10 14:55:24 -0700

    created AppControllerTest

---
+++ src/test/java/edu.sandiego.comp305/AppControllerTest.java
@@ -0,0 +1,28 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppControllerTest {
+
+    @Test
+    void handleServiceSelection() {
+    }
+
+    @Test
+    void handleSearch() {
+    }
+
+    @Test
+    void handlePayment() {
+    }
+
+    @Test
+    void handlePostListing() {
+    }
+
+    @Test
+    void handleJoinQueue() {
+    }
+}
