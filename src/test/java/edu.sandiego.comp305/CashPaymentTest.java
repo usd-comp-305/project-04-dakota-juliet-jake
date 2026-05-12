a6c4fb65678d7ea4a1e100104f2a1d70a5f27fce
Jake Coleman <wcoleman@sandiego.edu>
2026-05-12 14:58:39 -0700

    test: cashPayment test stubs created

---
+++ src/test/java/edu.sandiego.comp305/CashPaymentTest.java
@@ -0,0 +1,23 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashPaymentTest {
+
+    @Test
+    void cashPaymentReturnsTrueForValidAmount() {
+
+    }
+
+    @Test
+    void cashPaymentReturnsFalseForZeroAmount() {
+
+    }
+
+    @Test
+    void cashPaymentThrowsExceptionForNegativeAmount() {
+
+    }
+}
