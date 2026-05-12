7971a44e73d649cf4915ddb2b1327f8541d30389
Jake Coleman <wcoleman@sandiego.edu>
2026-05-12 15:56:00 -0700

    test: copied similar tests and implementation from CashPayment to CreditPayment

--- src/main/java/edu/sandiego/comp305/CreditCardPayment.java
+++ src/main/java/edu/sandiego/comp305/CreditCardPayment.java
@@ -3,12 +3,22 @@ package edu.sandiego.comp305;
 public class CreditCardPayment implements PaymentMethod{
     private final String cardNumber;
 
+    private static final double CENT_PRECISION = 100.0;
+
     public CreditCardPayment(final String cardNumber) {
         this.cardNumber = cardNumber;
     }
 
     @Override
     public boolean processPayment(final double amount){
-        return false;
+        if (amount < 0) {
+            throw new IllegalArgumentException(
+                    "Payment amount must be positive");
+        }
+        if (Math.round(amount * CENT_PRECISION) != amount * CENT_PRECISION) {
+            throw new IllegalArgumentException(
+                    "Payment amount cannot have more than 2 decimal places");
+        }
+        return true;
     }
 }
