708dc32025091a62fc08c3bc14a5c8f3bd3e6b5e
Jake Coleman <wcoleman@sandiego.edu>
2026-05-12 16:49:33 -0700

    test: copied similar tests and implementation from CashPayment to VemmoPayment

--- src/main/java/edu/sandiego/comp305/VenmoPayment.java
+++ src/main/java/edu/sandiego/comp305/VenmoPayment.java
@@ -3,12 +3,22 @@ package edu.sandiego.comp305;
 public class VenmoPayment implements PaymentMethod{
     private final String venmoHandle;
 
+    private static final double CENT_PRECISION = 100.0;
+
     public VenmoPayment(final String venmoHandle) {
         this.venmoHandle = venmoHandle;
     }
 
     @Override
     public boolean processPayment(final double amount) {
-        return false;
+        if (amount < 0) {
+            throw new IllegalArgumentException(
+                    "Payment amount must be positive");
+        }
+        if (Math.round(amount * CENT_PRECISION) != amount * CENT_PRECISION) {
+            throw new IllegalArgumentException(
+                    "Payment amount cannot have more than 2 decimal places");
+        }
+        return true;
     }
 }
---
+++ src/test/java/edu.sandiego.comp305/VenmoPaymentTest.java
@@ -0,0 +1,28 @@
+package edu.sandiego.comp305;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VenmoPaymentTest {
+
+    @Test
+    void processPaymentReturnsTrueForValidAmount() {
+        VenmoPayment payment = new VenmoPayment("@jake");
+        assertTrue(payment.processPayment(35.0));
+    }
+
+    @Test
+    void processPaymentThrowsExceptionForNegativeAmount() {
+        VenmoPayment payment = new VenmoPayment("@jake");
+        assertThrows(IllegalArgumentException.class, () ->
+                payment.processPayment(-20.0));
+    }
+
+    @Test
+    void processPaymentThrowsExceptionForImproperDecimals() {
+        VenmoPayment payment = new VenmoPayment("@jake");
+        assertThrows(IllegalArgumentException.class, () ->
+                payment.processPayment(50.123));
+    }
+}
