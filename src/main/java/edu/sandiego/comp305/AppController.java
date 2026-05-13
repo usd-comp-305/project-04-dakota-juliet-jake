add4c06ee2e625f13199e9ecb54e6cfd99c838ee
Jake Coleman <wcoleman@sandiego.edu>
2026-05-13 05:49:27 -0700

    refactor: handlePayment now keeps looping on itself when incorrect payment given

--- src/main/java/edu/sandiego/comp305/AppController.java
+++ src/main/java/edu/sandiego/comp305/AppController.java
@@ -36,16 +36,20 @@ public class AppController {
         final String username = terminalView.prompt("Enter your desired username:");
         final String password = terminalView.prompt("Create your password");
 
-        final String accountType = terminalView.prompt(
+        String accountType = terminalView.prompt(
                 "Great start! Now, would you like to create a Customer account or a " +
                         "Servicer account? (Type C for Customer and S for Servicer):");
 
-        if (accountType.equalsIgnoreCase("C")) {
-            handleCustomerFlow(name, username, password);
-        } else if (accountType.equalsIgnoreCase("S")) {
-            handleServicerFlow(name, username, password);
-        } else {
-            terminalView.display("Invalid selection. Please restart.");
+        while(true) {
+            if (accountType.equalsIgnoreCase("C")) {
+                handleCustomerFlow(name, username, password);
+                break;
+            } else if (accountType.equalsIgnoreCase("S")) {
+                handleServicerFlow(name, username, password);
+                break;
+            } else {
+                accountType = terminalView.prompt("Invalid selection. Please select C or S.");
+            }
         }
     }
 
@@ -61,7 +65,7 @@ public class AppController {
                 customer.setPassword(password);
                 break;
             } catch (IllegalArgumentException e) {
-                password = terminalView.prompt(e.getMessage());
+                password = terminalView.prompt(e.getMessage() + "Please re-enter it:");
             }
         }
 
@@ -219,35 +223,73 @@ public class AppController {
     }
 
     public void handlePayment(Customer customer) {
+        final double amountOwed = customer.getSelectedListing()
+                .getServiceOffered().getPrice();
+        terminalView.display("You owe $" + amountOwed +
+                ".");
         while (true) {
-            final double amount = Double.parseDouble(terminalView.prompt("Enter amount to pay:"));
-            PaymentMethod payment;
             final String paymentType = terminalView.prompt("Enter payment method (CREDIT/CASH/VENMO):");
-            final PaymentType type = PaymentType.valueOf(paymentType
-                    .toUpperCase());
-            if (type == PaymentType.CASH) {
-                payment = new CashPayment();
-                break;
-            } else if (type == PaymentType.CREDIT) {
-                final String cardNumber = terminalView.prompt("Enter your card number:");
-                payment = new CreditCardPayment(cardNumber);
-                break;
-            } else if (type == PaymentType.VENMO) {
-                final String venmoHandle = terminalView.prompt("Enter your Venmo handle:");
-                payment = new VenmoPayment(venmoHandle);
-                break;
-            } else {
+            PaymentMethod payment = null;
+            final PaymentType type;
+            try {
+                type = PaymentType.valueOf(paymentType
+                        .toUpperCase());
+                if (type == PaymentType.CASH) {
+                    payment = new CashPayment();
+                } else if (type == PaymentType.CREDIT) {
+                    boolean validLength = false;
+                    while (!validLength) {
+                        final String cardNumber = terminalView.prompt("Enter your card number:");
+                        payment = new CreditCardPayment(cardNumber);
+                        try {
+                            ((CreditCardPayment) payment).processPayment(amountOwed);
+                            validLength = true;
+                        } catch (IllegalArgumentException e) {
+                            terminalView.display(e.getMessage());
+                        }
+                    }
+
+                } else if (type == PaymentType.VENMO) {
+                    boolean validHandle = false;
+                    while (!validHandle) {
+                        final String venmoHandle = terminalView.prompt("Enter your Venmo handle");
+                        payment = new VenmoPayment(venmoHandle);
+                        try {
+                            ((VenmoPayment) payment).processPayment(amountOwed);
+                            validHandle = true;
+                        } catch (IllegalArgumentException e){
+                            terminalView.display(e.getMessage());
+                        }
+                    }
+
+                } else {
+                    terminalView.display("Please try again.");
+                    continue;
+                }
+            } catch (IllegalArgumentException e) {
                 terminalView.display("Invalid payment method.");
+                continue;
             }
-            final boolean paymentSuccess = customer.pay(amount, payment,
-                    customer.getSelectedListing().getServiceOffered());
-            if (paymentSuccess) {
-                terminalView.display("Payment successful!");
-                break;
-            } else {
-                terminalView.display("Payment failed. Please enter an amount " +
-                        "greater than or equal to the service cost.");
+
+            boolean validAmount = false;
+            while (!validAmount) {
+                final double amount = Double.parseDouble(terminalView.prompt("Please enter the amount " +
+                        "you would like to pay:"));
+                final boolean paymentSuccess = customer.pay(amount, payment,
+                        customer.getSelectedListing().getServiceOffered());
+                if (paymentSuccess) {
+                    terminalView.display("Payment successful! Thank you, " +
+                            "your payment has been accepted and your service " +
+                            "has been scheduled.");
+                    validAmount = true;
+                } else {
+                    terminalView.display("Payment failed. Please enter an " +
+                            "amount greater than or equal to " +
+                            "the service cost.");
+                }
             }
+            break;
+
         }
     }
 
